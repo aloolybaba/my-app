093afdab3d8e594f3ddf9d53f20a4f913c51b7f2
aloolybaba <149891636+aloolybaba@users.noreply.github.com>
2026-06-04 08:01:46 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,22 @@
 import { Events } from 'discord.js';
+import { postOrRefreshPanel } from '../handlers/panelHandler.js';
 import { log } from '../utils/logger.js';
 
 export default {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+
+  async execute(client) {
     log.info(`Ready as ${client.user.tag}`);
+
+    try {
+      const panelChannel = await client.channels.fetch(process.env.PANEL_CHANNEL_ID);
+      if (panelChannel) {
+        await postOrRefreshPanel(panelChannel, client.user.id);
+        log.info('[Ready] Panel check complete.');
+      }
+    } catch (error) {
+      log.warn('[Ready] Could not auto-post panel on startup:', error.message);
+    }
   },
 };
